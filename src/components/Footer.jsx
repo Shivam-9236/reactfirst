77f5c310e07a08b5b6db1c314469c5f88afad5ed
Shivam <shivamrdr6502@gmail.com>
2023-04-13 13:52:23 +0530

    Shivam Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div> 
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+        </div>
+
+        <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PLevs2qY7YS3wXu2qD2VsF835NdLKf2Inw&index=9&ab_channel=RickAstley" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/shivam_9236/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://www.youtube.com/watch?v=MtN1YnoL46Q&list=PLevs2qY7YS3wXu2qD2VsF835NdLKf2Inw&index=7&ab_channel=forrestfire101" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+
+
+    </footer>
+  )
+}
+
+export default Footer
